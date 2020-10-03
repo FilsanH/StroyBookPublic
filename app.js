d6de3f622d32989b8e0af04eec0968bdc3b162c8
Filsan Hassan <fh363.cam.ac.uk>
2020-10-03 19:57:17 +0100

    Auth middleware and google login

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const connect = require('mongoose')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 
 //Load config
 dotenv.config({ path: './config/config.env' }) // put all of our global variables
+
+// Passport config
+require('./config/passport')(passport)
+// connect to DB
+connectDB()
+
 const app = express()
+
+//logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')) //https request looger middleware
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+// layouts works by inputing 'views' files into the body of layout
+
+//Session middleware must go above passport middleware
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false, //don't create a session untill its called
+    cookie: { secure: true },
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// define folder as Static folder so files can be access from the client
+app.use(express.static(path.join(__dirname, '/public')))
+//Routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 5000 //.env access defined variables
 
