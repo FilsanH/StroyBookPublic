0be54fb22799485e2bbbbb236745c84ceb78de12
Filsan Hassan <fh363.cam.ac.uk>
2020-10-03 15:00:14 +0100

    dev and prod mode

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
-//Load config 
-dotenv.config({path: './config/config.env'})
+//Load config
+dotenv.config({ path: './config/config.env' }) // put all of our global variables
 const app = express()
+
+const PORT = process.env.PORT || 5000 //.env access defined variables
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
