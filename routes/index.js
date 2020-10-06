7ebba062a0bbfb1e3fb6c020cf720b47e6a7376a
Filsan Hassan <fh363.cam.ac.uk>
2020-10-06 18:03:13 +0100

    dashboard story model

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Story = require('../models/Story')
+
 // @desc    Login/Landing Page
 // @route   GET
 
@@ -15,8 +17,17 @@ router.get('/', ensureGuest, (req, res) => {
 // @desc    Login/Landing Page
 // @route   GET
 
-router.get('/dashboard', ensureAuth, (req, res) => {
-  res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.findOne({ user: req.user.id }).lean() // returns a javascript object rather than a mongoose
+    res.render('dashboard', {
+      name: req.user.firstName,
+      stories: stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
 })
 
 module.exports = router
