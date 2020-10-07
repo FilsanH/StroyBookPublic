1801d48850fba59b139991b3610fd270256cc449
Filsan Hassan <fh363.cam.ac.uk>
2020-10-07 11:56:46 +0100

    delete  route sotries

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(function (req, res, next) {
   next()
 })
 // define folder as Static folder so files can be access from the client
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public/')))
 
 //Routes
 app.use('/', require('./routes/index'))
--- routes/stories.js
+++ routes/stories.js
@@ -22,21 +22,28 @@ router.post('/', ensureAuth, async (req, res) => {
     res.render('error/500')
   }
 })
-///@desc Show add page
-//@route GET /stories/edit/id:
+// @desc    Show edit page
+// @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  const story = await Story.findOne({
-    _id: req.params.id,
-  }).lean()
-  if (!story) {
-    return res.render('error/404')
-  }
-  if (story.user != req.user.id) {
-    res.redirect('/stories')
-  } else {
-    res.render('stories/edit', {
-      story,
-    })
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      res.render('stories/edit', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
   }
 })
 
@@ -57,22 +64,50 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
-///@desc Update story
-//@route PUT /stories//:id
+// @desc    Update story
+// @route   PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean()
-  if (!story) {
-    return res.render('error/404')
-  }
-  if (story.user != req.user.id) {
-    res.redirect('/stories')
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true, // creates a new one if it doesn't exists
-      runValidators: true, // checks that mongoose fields are valid
-    })
-    res.redirect('/dashboard')
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // creates a new one if it doesn't exists
+        runValidators: true, // checks that mongoose fields are valid
+      })
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
   }
 })
 
+// @desc    Delete story
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      await Story.remove({ _id: req.params.id })
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
 module.exports = router
