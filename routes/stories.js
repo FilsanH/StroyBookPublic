daf35bd5cebc6dd6e69c09ba8aa244d29f208d54
Filsan Hassan <fh363.cam.ac.uk>
2020-10-07 12:06:59 +0100

    single story page and user story page

--- routes/stories.js
+++ routes/stories.js
@@ -89,6 +89,29 @@ router.put('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user._id != req.user.id && story.status == 'private') {
+      res.render('error/404')
+    } else {
+      res.render('stories/show', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 // @desc    Delete story
 // @route   DELETE /stories/:id
 router.delete('/:id', ensureAuth, async (req, res) => {
@@ -111,3 +134,22 @@ router.delete('/:id', ensureAuth, async (req, res) => {
   }
 })
 module.exports = router
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean()
+
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
